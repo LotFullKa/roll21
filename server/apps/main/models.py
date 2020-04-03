4ceab8b92e5d5a9ee6d164935f1b2c54cc6aa679
Kamil <lotfullin.kr@phystech.edu>
2020-04-03 20:35:59 +0300

    little correct

--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from random import randint
 
 
 class Subject(models.Model):
@@ -8,7 +10,7 @@ class Subject(models.Model):
     y_pos = models.SmallIntegerField()
     hp = models.SmallIntegerField(default=100)
     is_dead = models.BooleanField(default=False)
-    type_of_subject = models.CharField()
+    type_of_subject = models.CharField(max_length=50)
 
     COLOR_RED = "red"
     COLOR_BLUE = "blue"
@@ -20,9 +22,25 @@ class Subject(models.Model):
         (COLOR_GREEN, "зеленый"),
     )
 
-    color_choices = models.CharField(choices=colors)
+    color_choices = models.CharField(choices=colors, max_length=50)
 
     def move(self, new_x, new_y):
         self.x_pos = new_x
         self.y_pos = new_y
         self.save()
+
+    def save(self, **kwargs):
+        if self._state.adding:
+            self.x_pos, self.y_pos = self.generate_pos()
+        super().save(**kwargs)
+
+    @staticmethod
+    def generate_pos():
+        # TODO: @Kamil control recursion depth
+        x = randint(0, settings.FIELD_SIZE)
+        y = randint(0, settings.FIELD_SIZE)
+
+        if not Subject.objects.exists(x_pos=x, y_pos=y):
+            return x, y
+        else:
+            return Subject.generate_pos()
