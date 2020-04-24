f4c7c8b6186ba80eb3e2ced9af965a77aac844a2
Sergey Shevchenko <sergey.shevchenko@dteam.dev>
2020-04-24 23:55:21 +0300

    Additional typings for enum_helpers.py

--- server/utils/enum_helpers.py
+++ server/utils/enum_helpers.py
@@ -1,21 +1,21 @@
 import random
 from enum import Enum
-from typing import Type
+from typing import Type, List, Tuple
 
 
-def enum_to_choices(enum: Type[Enum]):
+def enum_to_choices(enum: Type[Enum]) -> List[Tuple]:
     """Convert enum to django choices."""
 
     return [(item, item.value) for item in enum]
 
 
-def enum_to_key_list(enum: Type[Enum]):
+def enum_to_key_list(enum: Type[Enum]) -> List[str]:
     """Convert enum to list of keys."""
 
     return [item for item in enum]
 
 
-def random_enum_key(enum: Type[Enum]):
+def random_enum_key(enum: Type[Enum]) -> str:
     """Return random enum key."""
 
     return random.choice(enum_to_key_list(enum))
