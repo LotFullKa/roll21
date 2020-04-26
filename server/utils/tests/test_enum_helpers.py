3ca7b7a392431351de0bb855336058c9c9bec485
Sergey Shevchenko <sergey.shevchenko@dteam.dev>
2020-04-27 00:04:38 +0300

    Fixed Enum tests

--- server/utils/tests/test_enum_helpers.py
+++ server/utils/tests/test_enum_helpers.py
@@ -13,13 +13,13 @@ class EnumHelpersTestCase(unittest.TestCase):
     def test_enum_to_choices(self):
         choices = enum_to_choices(self.TestEnum)
 
-        self.assertEqual(choices[0], (self.TestEnum.key1, self.TestEnum.key1.value))
-        self.assertEqual(choices[1], (self.TestEnum.key2, self.TestEnum.key2.value))
+        self.assertEqual(choices[0], (self.TestEnum.key1.name, self.TestEnum.key1.value))
+        self.assertEqual(choices[1], (self.TestEnum.key2.name, self.TestEnum.key2.value))
 
     def test_enum_to_key_list(self):
         key_list = enum_to_key_list(self.TestEnum)
-        self.assertEqual(key_list, [self.TestEnum.key1, self.TestEnum.key2])
+        self.assertEqual(key_list, [self.TestEnum.key1.name, self.TestEnum.key2.name])
 
     def test_random_enum_key(self):
         with mock.patch('utils.enum_helpers.random.choice', lambda choices: choices[0]):
-            self.assertEqual(random_enum_key(self.TestEnum), self.TestEnum.key1)
+            self.assertEqual(random_enum_key(self.TestEnum), self.TestEnum.key1.name)
